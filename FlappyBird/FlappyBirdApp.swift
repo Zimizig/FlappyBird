e21532afd663b5e34bda0d7cab3f07c28274b88d
Zimizig <ramazanow9999@gmail.com>
2024-07-29 22:09:27 +0500

    Initial Commit

---
+++ FlappyBird/FlappyBirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlappyBirdApp.swift
+//  FlappyBird
+//
+//  Created by Роман on 29.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlappyBirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
